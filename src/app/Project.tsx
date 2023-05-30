7246026a03f185d159e9805126ca22d2c2093ad6
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-30 09:33:23 -0500

    Project cards mockup created and functioning

---
+++ src/app/Project.tsx
@@ -0,0 +1,3 @@
+export default function Project() {
+  return <p className="w-52 h-52 border">Testing Project Card</p>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,49 @@
 import Image from 'next/image';
 import Description from './Description';
 import me from '../../public/walking-me.png';
+import Project from './Project';
 
 export default function Home() {
   return (
-    <div className="flex flex-col-reverse sm:flex-row align-middle h-96 my-20 justify-center">
-      <div
-        id="left-side"
-        className="flex flex-col items-center gap-4 justify-center"
-      >
-        <h2 className=" sm:text-7xl text-4xl font-bold tracking-wider">
-          Victoria
-        </h2>
-        <h2 className="sm:text-7xl text-4xl font-bold tracking-wider">
-          {' '}
-          Mertens
-        </h2>
-        <div className="flex flex-col items-center">
-          <p className="flex justify-items-center sm:text-2xl text-lg p-5 font-bold">
-            Software Engineer
-          </p>
-          <p className=" md:text-xl text-base text-center w-[390px]">
-            Building applications that are <Description />
-          </p>
+    <div>
+      <div className="flex flex-col-reverse sm:flex-row align-middle h-96 my-20 justify-center">
+        <div
+          id="left-side"
+          className="flex flex-col items-center gap-4 justify-center"
+        >
+          <h2 className=" sm:text-7xl text-4xl font-bold tracking-wider">
+            Victoria
+          </h2>
+          <h2 className="sm:text-7xl text-4xl font-bold tracking-wider">
+            {' '}
+            Mertens
+          </h2>
+          <div className="flex flex-col items-center">
+            <p className="flex justify-items-center sm:text-2xl text-lg p-5 font-bold">
+              Software Engineer
+            </p>
+            <p className=" md:text-xl text-base text-center w-[390px]">
+              Building applications that are <Description />
+            </p>
+          </div>
+        </div>
+
+        <div
+          id="right-side"
+          className="flex flex-col items-center max-h-70 -mr-10"
+        >
+          <Image
+            src={me}
+            // width={300}
+            alt="Cartoon of Victoria holding MacBook and coffee"
+            className="sm:w-72 w-52"
+          />
         </div>
       </div>
-
-      <div
-        id="right-side"
-        className="flex flex-col items-center max-h-70 -mr-10"
-      >
-        <Image
-          src={me}
-          // width={300}
-          alt="Cartoon of Victoria holding MacBook and coffee"
-          className="sm:w-72 w-52"
-        />
+      <div className="flex justify-around">
+        <Project />
+        <Project />
+        <Project />
       </div>
     </div>
   );
