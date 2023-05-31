ff2c8129000c162ec28347caff3688260fe2b9d7
Victoria Mertens <victoria.mertens44@gmail.com>
2023-05-31 09:19:00 -0500

    Accessibility update for button

--- src/app/ProjectHover.tsx
+++ src/app/ProjectHover.tsx
@@ -4,7 +4,10 @@ export default function ProjectHover(props) {
       <p className="text-center  text-lg text-white opacity-100 z-10">
         {props.des}
       </p>
-      <button className="border border-solid p-2 px-5 rounded-3xl m-4 text-blue-300">
+      <button
+        role="link"
+        className="border border-solid p-2 px-5 rounded-3xl m-4 text-blue-300"
+      >
         Go to Project
       </button>
     </div>
